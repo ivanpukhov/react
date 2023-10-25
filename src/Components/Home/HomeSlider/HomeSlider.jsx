b6acd9dd6f08da9057e8d1bbdec6994b60ec303d
ivan <ix.pukhov@gmail.com>
2023-10-26 00:54:31 +0600

    d

--- src/Components/Home/HomeSlider/HomeSlider.jsx
+++ src/Components/Home/HomeSlider/HomeSlider.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import action from "../../../assets/img/action.webp";
-import banner from "../../../assets/img/banner.webp";
+import action from "../../../assets/img/action.svg";
+import banner from "../../../assets/img/banner.svg";
 
 const HomeSlider = () => {
     return(
