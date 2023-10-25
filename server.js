3f51dfca923d3155c49014ac8ea855310162cf8f
ivan <ix.pukhov@gmail.com>
2023-10-25 23:13:02 +0600

    sitemap

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const path = require('path');
+const expressSitemapXml = require("express-sitemap-xml");
+
 const app = express();
+
+// Функция для генерации URL'ов для sitemap
+const buildSitemapUrls = () => [
+  { url: '/cosmetics', changefreq: 'daily', priority: 0.8 },
+  { url: '/chimiya', changefreq: 'daily', priority: 0.8 },
+  { url: '/products', changefreq: 'daily', priority: 0.8 },
+  { url: '/head', changefreq: 'daily', priority: 0.8 },
+  { url: '/bads', changefreq: 'daily', priority: 0.8 },
+  { url: '/catalog', changefreq: 'daily', priority: 0.8 },
+  { url: '/', changefreq: 'daily', priority: 1.0 },
+  // ... дополнительные URL
+];
+
+// Подключение sitemap к приложению
+app.use(
+    '/sitemap.xml',
+    expressSitemapXml(buildSitemapUrls, 'https://miko-astana.kz')
+);
 
 // Укажите путь к собранным файлам
 app.use(express.static(path.join(__dirname, 'build')));
@@ -14,4 +34,3 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
-
